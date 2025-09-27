9534a954347033422d56de13ea8c631a457cf8a0
Alle4n <allennajjuma96@gmail.com>
2025-09-27 17:37:48 +0300

    PWA configuration

--- hirevo/pages/_document.tsx
+++ hirevo/pages/_document.tsx
@@ -3,7 +3,21 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content="#4f46e5" />
+
+        {/* Manifest file */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Favicon & icons */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        
+        {/* Optional: iOS splash screen background */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
